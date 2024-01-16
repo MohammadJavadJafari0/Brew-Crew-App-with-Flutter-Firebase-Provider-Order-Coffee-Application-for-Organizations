cd6b312a2577b5c264a089f3288623f6ce7e9e7e
MohammadJavadJafari0 <mohammadjavad.jafari1@gmail.com>
2024-01-16 21:16:05 +0330

    1 : first settings like firebase install,screens (wrapper,home,authenticate

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
